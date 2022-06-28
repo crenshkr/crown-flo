affa00f99fb372ae469b0b3886a7f8916d1d4498
crenshkr <amir.crenshaw@gmail.com>
2022-06-28 15:27:34 -0400

    made responsive

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function on() {
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
+
+  function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
